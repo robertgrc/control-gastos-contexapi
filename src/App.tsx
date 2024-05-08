539083be67464a07dcff76c78e5c71078eac2bb3
robertgrc <robert.rodriguez.rgrc@gmail.com>
2024-05-08 18:50:56 -0400

    Instalando Headless para la ventana modal y creando ExpenseModal.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useMemo } from "react"
 import BudgetForm from "./components/BudgetForm"
 import { useBudget } from "./hooks/useBudget"
 import BudgetTracker from "./components/BudgetTracker"
+import ExpenseModal from "./components/ExpenseModal"
 
 // 
 
@@ -24,6 +25,12 @@ function App() {
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
         {isValidBudget ?<BudgetTracker/> :<BudgetForm />}
       </div>
+
+      {isValidBudget&& (
+        <main className="max-w-3xl mx-auto py-10">
+          <ExpenseModal />
+        </main>
+      )}
     </>
   )
 }
