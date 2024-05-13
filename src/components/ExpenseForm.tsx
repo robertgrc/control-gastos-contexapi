ee8a645c37abd3e9503d70001120c351fca5434d
robertgrc <robert.rodriguez.rgrc@gmail.com>
2024-05-13 16:04:42 -0400

    Mostrar de forma condicional si es gasto nuevo o edicion

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -73,7 +73,7 @@ const handleSubmit =(e:React.FormEvent<HTMLFormElement>)=>{
         <legend
             className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2"
         >
-            Nuevo Gasto
+            {state.editingId? 'Guardar Cambios': 'Nuevo Gasto'}
         </legend>
         {error && <ErrorMessage>{error}</ErrorMessage> }
         <div className="flex flex-col gap-2">
@@ -155,7 +155,7 @@ const handleSubmit =(e:React.FormEvent<HTMLFormElement>)=>{
         <input 
             type="submit"
             className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg" 
-            value={'Registrar Gasto'}
+            value ={state.editingId? 'Guardar Cambios': 'Registrar Gasto'}
         />
    </form>
   )
