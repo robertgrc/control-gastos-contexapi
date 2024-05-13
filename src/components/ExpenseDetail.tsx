0aa6aa24fbea29a6ea07e6ded0b4729727ee5df1
robertgrc <robert.rodriguez.rgrc@gmail.com>
2024-05-13 12:49:34 -0400

    mostando los iconos de la categoria del gasto

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -1,18 +1,29 @@
+import { useMemo } from "react"
 import { formatDate } from "../helpers"
 import { Expense } from "../types"
 import AmountDisplay from "./AmountDisplay"
+import { categories } from "../data/categories"
 
 type ExpenseDetailProps = {
     expense: Expense
 }
 
 export default function ExpenseDetail({expense}:ExpenseDetailProps) {
+
+  const categoryInfo = useMemo(() => categories.filter(cat => cat.id ===expense.category)[0] , [expense])
+  console.log(categoryInfo)
+
   return (
     <div className="bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 items-center">
-      {/* <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-        
-      </div> */}
       <div>
+        <img
+          src={`/icono_${categoryInfo.icon}.svg`}
+          alt="icono gasto"
+          className="w-20"
+        />
+      </div>
+      <div className="flex-1 space-y-2">
+        <p className="text-sm font-bold uppercase text-slate-500">{categoryInfo.name}</p>
         <p>{expense.expenseName}</p>
         <p className="text-slate-600 text-sm">{formatDate(expense.date!.toString())}</p>
       </div>
